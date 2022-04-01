3a9fcf207478ead8c646565109b132d8760df920
Gabriella <briblack.95@outlook.com>
2022-04-01 11:34:30 +0200

    initial commit

---
+++ IconsGenerator.py
@@ -0,0 +1,27 @@
+import glob
+from PIL import Image
+
+AndroidIconSizes = [36,48,72,96]
+iOSIconSizes = [57,72,76,114,120,144,152,167,180,1024]
+
+def generateAndroidIcons(image):
+    for size in AndroidIconSizes:
+        outFilePath = "output/android/"+str(size) + "x" + str(size)+".png"
+        resizedImage = image.resize((size, size), Image.ANTIALIAS)
+        resizedImage.save(outFilePath,"PNG")
+
+def generateIosIcons(image):
+    for size in iOSIconSizes:
+        outFilePath = "output/iOS/"+str(size) + "x" + str(size)+".png"
+        resizedImage = image.resize((size, size), Image.ANTIALIAS)
+        resizedImage.save(outFilePath,"PNG")
+
+def generateIcons():
+    print(glob.glob("input/*.png"))
+    iconPathsArray = glob.glob("input/*.png")
+    for iconPath in iconPathsArray:
+        with Image.open(iconPath) as image:
+            generateAndroidIcons(image)
+            generateIosIcons(image)
+
+generateIcons()
---
+++ readme.md
@@ -0,0 +1,7 @@
+This Script creates resized icons in the required android and iOS resolutions
+
+1. Add the biggest size of the icon (or icons) in the input folder
+2. create virtualenv: "python -m venv [pathtoenv]"
+3. activate virtualenv: "source venv/Scripts/activate"
+4. install requirements: "pip install -r requirements.txt"
+5. Run the program: "python IconsGenerator.py"
